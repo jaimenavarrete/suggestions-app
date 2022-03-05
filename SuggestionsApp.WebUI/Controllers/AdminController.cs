a7e66e5e51019ff11a8d6dada8e6993f26dc2f23
Jaime Navarrete <jaimenava123@outlook.com>
2022-03-05 09:13:47 -0600

    Agrega dashboard del administrador

---
+++ SuggestionsApp.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuggestionsApp.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoriesList()
+        {
+            return View();
+        }
+
+        public IActionResult StatesList()
+        {
+            return View();
+        }
+
+        public IActionResult UsersList()
+        {
+            return View();
+        }
+    }
+}
---
+++ SuggestionsApp.WebUI/Views/Shared/ManageAdminNavPages.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SuggestionsApp.WebUI.Views.Shared
+{
+    public static class ManageAdminNavPages
+    {
+        public static string SuggestionsApproval => "SuggestionsApproval";
+
+        public static string Index => "Index";
+
+        public static string CategoriesList => "CategoriesList";
+
+        public static string StatesList => "StatesList";
+
+        public static string UsersList => "UsersList";
+
+
+        public static string SuggestionsApprovalNavClass(ViewContext viewContext) => PageNavClass(viewContext, SuggestionsApproval);
+
+        public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+
+        public static string CategoriesListNavClass(ViewContext viewContext) => PageNavClass(viewContext, CategoriesList);
+
+        public static string StatesListNavClass(ViewContext viewContext) => PageNavClass(viewContext, StatesList);
+
+        public static string UsersListNavClass(ViewContext viewContext) => PageNavClass(viewContext, UsersList);
+
+
+        private static string PageNavClass(ViewContext viewContext, string page)
+        {
+            var activePage = viewContext.ViewData["ActivePage"] as string
+                ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+        }
+    }
+}
