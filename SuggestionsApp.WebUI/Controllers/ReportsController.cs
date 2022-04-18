252c8ee8c6978883b36e263fe52982f3e75a8aab
Jaime Navarrete <jaimenava123@outlook.com>
2022-04-18 17:47:21 -0600

    Agrega diseño y funcionalidad para imprimir reportes de sugerencias aprobadas y pendientes

--- SuggestionsApp.WebUI/Controllers/ReportsController.cs
+++ SuggestionsApp.WebUI/Controllers/ReportsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Rotativa.AspNetCore;
 using SuggestionsApp.Models.Interfaces;
 using SuggestionsApp.WebUI.ViewModels;
@@ -10,21 +11,38 @@ namespace SuggestionsApp.WebUI.Controllers
     [Authorize(Roles = "Admin,Moderador")]
     public class ReportsController : Controller
     {
+        private readonly ISuggestionsService _suggestionsService;
         private readonly ICategoriesService _categoriesService;
         private readonly IStatesService _statesService;
         private readonly IUserService _userService;
         private readonly IMapper _mapper;
 
         public ReportsController(
+            ISuggestionsService suggestionsService,
             ICategoriesService categoriesService,
             IStatesService statesService,
             IUserService userService,
             IMapper mapper)
         {
+            _suggestionsService = suggestionsService;
             _categoriesService = categoriesService;
             _statesService = statesService;
             _userService = userService;
             _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PrintApprovedSuggestions()
+        {
+            var approvedSuggestionsViewModel = await GetSuggestionsViewModel(isApproved: true);
+            return CreatePdf("PrintSuggestions", approvedSuggestionsViewModel, "Reporte de sugerencias aprobadas");
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> PrintPendingSuggestions()
+        {
+            var pendingSuggestionsViewModel = await GetSuggestionsViewModel(isApproved: null);
+            return CreatePdf("PrintSuggestions", pendingSuggestionsViewModel, "Reporte de sugerencias pendientes");
         }
 
         [HttpGet]
@@ -60,9 +78,31 @@ namespace SuggestionsApp.WebUI.Controllers
 
         #region HelperMethods
 
+        private async Task<List<PendingSuggestionViewModel>> GetSuggestionsViewModel(bool? isApproved)
+        {
+            var suggestions = await _suggestionsService.GetSuggestions(isApproved);
+            var suggestionsViewModel = _mapper.Map<List<PendingSuggestionViewModel>>(suggestions);
+
+            foreach (var suggestion in suggestionsViewModel)
+            {
+                suggestion.UserName = await _userService.GetUserNameById(suggestion.UserId);
+                suggestion.Description = suggestion.Description?.Length > 250 ? $"{suggestion.Description[..250]}..." : suggestion.Description;
+            }
+
+            return suggestionsViewModel;
+        }
+
         private ViewAsPdf CreatePdf(object list)
         {
             return new ViewAsPdf(list)
+            {
+                CustomSwitches = "--page-offset 0 --footer-center [page] --footer-font-size 10"
+            };
+        }
+        
+        private ViewAsPdf CreatePdf(string viewName, object list, string title)
+        {
+            return new ViewAsPdf(viewName, list, new ViewDataDictionary(ViewData) { { "Title", title } })
             {
                 CustomSwitches = "--page-offset 0 --footer-center [page] --footer-font-size 10"
             };
